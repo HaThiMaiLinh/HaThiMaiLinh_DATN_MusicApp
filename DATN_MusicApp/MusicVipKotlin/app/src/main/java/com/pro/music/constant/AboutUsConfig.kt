51b223cbe38b6ef93d32f179207f3be3d71ed4de
mailinh <mailinh28022003@gmail.com>
2024-12-23 00:34:30 +0700

    completed datn

---
+++ DATN_MusicApp/MusicVipKotlin/app/src/main/java/com/pro/music/constant/AboutUsConfig.kt
@@ -0,0 +1,19 @@
+package com.pro.music.constant
+
+interface AboutUsConfig {
+    companion object {
+        const val ABOUT_US_TITLE = "HTML"
+        const val ABOUT_US_CONTENT = "Tại HTML Music App, chúng tôi tin rằng âm nhạc không chỉ là âm thanh – đó là nhịp điệu của cuộc sống và giai điệu của những kỷ niệm." +
+                " Hãy cùng chúng tôi tiến gần hơn đến những bài hát bạn yêu thích, tạo nên những khoảnh khắc đáng nhớ mãi mãi."
+        const val ABOUT_US_SLOGAN = "Mang thế giới âm nhạc đến gần bạn – nơi mỗi nốt nhạc kể một câu chuyện của bạn."
+        const val ABOUT_US_WEBSITE_TITLE = "HTML Website"
+        const val PAGE_FACEBOOK = "https://www.facebook.com/groups/fallinluv.music"
+        const val LINK_FACEBOOK = "https://www.facebook.com/htmlinh28/"
+        const val LINK_YOUTUBE = "https://youtu.be/Jn4RgJ32Y-w?si=RK42rEItCBRZOdXG"
+        const val PHONE_NUMBER = "+84368682709"
+        const val GMAIL = "mailinh28082003@gmail.com"
+        const val SKYPE_ID = "mailinh2808"
+        const val ZALO_LINK = "https://zalo.me/0368682709"
+        const val WEBSITE = "https://soundcloud.com/ngot"
+    }
+}
